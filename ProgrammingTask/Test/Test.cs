e01e2b12b10608e86e604165581be5ccddc493bc
psiblvdegod <artyombaly@list.ru>
2025-08-12 13:03:03 +0300

    add simple test

---
+++ ProgrammingTask/Test/Test.cs
@@ -0,0 +1,19 @@
+﻿namespace ProgrammingTask.Tests;
+
+using ProgrammingTask.Lib;
+
+[TestFixture]
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        List<int> numbers = [6, 2, 7, 4];
+
+        InsertionSort.SortEven(numbers);
+
+        List<int> expectedResult = [2, 4, 7, 6];
+
+        Assert.That(numbers.SequenceEqual(expectedResult));
+    }
+}
